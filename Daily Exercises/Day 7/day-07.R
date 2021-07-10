91b604164d94841f889bfe3b5b0c5bbd9c467eee
Anisah Calvin Cuevas <anisahc2000.3@gmail.com>
2021-07-10 16:09:53 -0700

    updates

---
+++ Daily Exercises/Day 7/day-07.R
@@ -0,0 +1,8 @@
+#Anisah Calvin Cuevas
+#July 4 2021
+#Daily Exercise 7
+
+library(tidyverse)
+library(zoo)
+url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties-recent.csv'
+covid = read_csv(url)
